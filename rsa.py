b6d9427f5e8a57a4eaf3d95ef5377d2eedbdbf5c
clasarte <constanzalasarte44@gmail.com>
2025-04-11 18:42:32 -0300

    encrypt method and main example

--- rsa.py
+++ rsa.py
@@ -22,3 +22,30 @@ class RSAKeyPair:
 
         self.public_key = (self.n, self.e)
         self.private_key = self.d
+    def __str__(self):
+        return f"RSAKeyPair(p={self.p}, q={self.q}, n={self.n}, phi={self.phi}, e={self.e}, d={self.d}, public_key={self.public_key}, private_key={self.private_key})"
+
+def encrypt(message, public_key):
+    n, e = public_key
+
+    return pow(message, e, n)
+
+def generate_keypair(p, q):
+    return RSAKeyPair(p, q)
+
+if __name__ == "__main__":
+    p = 61  # Example prime number
+    q = 53  # Another example prime number
+
+    key_pair = generate_keypair(p, q)
+    print(key_pair)
+    public_key = key_pair.public_key
+    private_key = key_pair.private_key
+    n = key_pair.n
+
+    message = 123
+    print(f"Original Message: {message}")
+
+    ciphertext = encrypt(message, public_key)
+    print(f"Ciphertext: {ciphertext}")
+
